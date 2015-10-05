5315b30af46526621f2e70e7fbe9594f9c830d69
Kelvin <hello@iamkelv.in>
2015-10-05 18:38:59 +0100

    Add Question 2.py

---
+++ 1. IF Statements/Question 2.py
@@ -0,0 +1,11 @@
+# Question 2
+# Author: Kelvin Zhang
+# Date Created: 2015-10-15
+
+# Prompt for user input
+testResult = int(input("What is the test result of the student? "))
+
+if testResult <= 60 and testResult <= 80:
+    print("The student is allowed into the school.")
+else:
+    print("The student is not allowed into the school.")
