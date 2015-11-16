7b83e027ed29c3e6a159ea81e72c3d30448b690c
Kelvin <hello@iamkelv.in>
2015-11-16 18:20:16 +0000

    Complete array homework

--- 2. Two Dimensional Arrays/Question1.py
+++ 2. Two Dimensional Arrays/Question1.py
@@ -2,3 +2,52 @@
 # Author: Kelvin Zhang
 # Date Created: 2015-11-16
 
+import random
+
+"""
+Helper functions
+"""
+
+
+def unique_list_generator():
+    """
+    Generates a list of six unique, randomly chosen integers between 1 and 49 in ascending order
+    :return: random_list
+    """
+    random_list = []
+    for i in range(0, 6):
+        while True:
+            rand_int = random.randint(1, 49)
+            if rand_int in random_list:
+                continue
+            else:
+                random_list.append(rand_int)
+                break
+
+    # Sort list into ascending order
+    random_list.sort()
+
+    return random_list
+
+
+"""
+Main code
+"""
+
+# Generate random list to compare against
+masterList = unique_list_generator()
+
+# Output master list to user
+print("Initial list of numbers:")
+print(masterList)
+
+# Count the loops needed to generate the same set of numbers in the master list
+loopsMade = 0
+while True:
+    loopsMade += 1
+
+    if masterList == unique_list_generator():
+        break
+
+# Output the number of loops made to user
+print("{!s} loops were made to match the list of numbers.".format(loopsMade))
